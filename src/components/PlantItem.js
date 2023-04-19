b014ee8105b58d86dd8d2c78e06489a726e3d3f2
Ayoub <ayoub.slimanipro@gmail.com>
2023-04-19 09:16:02 +0200

    ajout component

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -2,8 +2,9 @@ import CareScale from './CareScale'
 import '../styles/PlantItem.css'
 
 function PlantItem({ id, cover, name, water, light }) {
+
 	return (
-		<li key={id} className='lmj-plant-item'>
+		<li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
@@ -12,6 +13,10 @@ function PlantItem({ id, cover, name, water, light }) {
 			</div>
 		</li>
 	)
+}
+
+function handleClick (e) {
+	console.log('✨ Ceci est mon event :', e)
 }
 
 export default PlantItem
